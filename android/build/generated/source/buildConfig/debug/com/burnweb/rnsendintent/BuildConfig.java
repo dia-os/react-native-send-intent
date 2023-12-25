2049dcc6a1bd3013d3f0faa01c36bf7cfd71db38
yunus emre <yunusemreayarr@gmail.com>
2023-12-25 11:02:34 +0300

    Zebra scanner açılması için ekleme yapılmıştır.

---
+++ android/build/generated/source/buildConfig/debug/com/burnweb/rnsendintent/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.burnweb.rnsendintent;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.burnweb.rnsendintent";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 28;
+  public static final String VERSION_NAME = "1.3.0";
+}
